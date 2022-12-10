932454327fb14fb4b4a4ac5f7c23d801f09395b7
Dominik Grzegorzek <grzegor.dominik@gmail.com>
2022-12-10 12:26:12 +0100

    Day 10: Cathode-Ray Tube

---
+++ 2022/10.rb
@@ -0,0 +1,174 @@
+input = DATA.read().split("\n").map(&:split)
+
+row = Array.new(40)
+cycles = 0
+image = []
+res = []
+x = 1
+
+input.each { |op, arg|
+  op_cycles = op == "addx" ? 2 : 1
+
+  (op_cycles).times {
+    row[cycles % 40] = (cycles % 40).between?(x - 1, x + 1) ? "#" : "."
+
+    cycles += 1
+    image << row.map(&:clone) if (cycles % 40) == 0
+
+    res << (cycles * x) if cycles % 40 == 20
+  }
+
+  x += arg.to_i if op == "addx"
+}
+
+p res.sum
+
+image.each { |row| p row.join }
+
+__END__
+addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop
